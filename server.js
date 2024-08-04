1da23960bbe328b88359f4f78eaa9eaac9089357
Naitikraj98 <166008969+Naitikraj98@users.noreply.github.com>
2024-08-04 12:04:22 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
-const TelegramBot = require('node-telegram-bot-api');
-const request = require('request');
-const token = '7025543734:AAH-chmUhJazD4z2cY9Xv2TdIYIdOWDjgdQ';
-const bot = new TelegramBot(token, {polling: true});
+const TeleBot = require('node-telegram-bot-api');
+const Token = '7025543734:AAH-chmUhJazD4z2cY9Xv2TdIYIdOWDjgdQ';
+const bot = new TeleBot(Token, {polling: true});
 
-bot.onText(/\/movie (.+)/, function(msg, match) {
+var request = require('request');
+
+bot.onText(/\/movie (.+)/,function(msg, match) {
     var chatId = msg.chat.id;
     var movie = match[1];
 
-    request(`https://www.omdbapi.com/?t=${movie}&apikey=50393264`, function(error, response, body) {
+    request(`https://www.omdbapi.com/?s=movie%20title&apikey=50393264`, function(error, response, body) {
         if (!error && response.statusCode == 200) {
             bot.sendMessage(chatId, `_Looking for_ `+ movie +`...`, {parse_mode: 'Markdown'})
             .then(function(msg) {
                 var res = JSON.parse(body);
-                if (res.Response === "True") {
-                    bot.sendPhoto(chatId, res.Poster, {caption: `Result:\nTitle: ${res.Title}\nYear: ${res.Year}\nRated: ${res.Rated}\nReleased: ${res.Released}`});
-                } else {
-                    bot.sendMessage(chatId, `Movie not found!`);
-                }
-            });
-        } else {
-            bot.sendMessage(chatId, `Error: ${error}`);
-        }
+               bot.sendPhoto(chatId, res.Poster,{caption:'Result:\nTitle: '+res.Title+ '\nYear:'+res.Year +'\nRated:'+res.Rated+ '\nReleased:'+res.Released})
+            })
+        } 
     });
 });
+
